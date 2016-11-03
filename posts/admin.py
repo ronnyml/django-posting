92ab597359e110074d8f8d62f960af8edf9870a2
Ronny Yabar <ronny@kde.org>
2016-11-03 05:25:46 -0500

    Add CategoryAdmin to admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Category, Post
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        'title', 'slug', 'posts_count', 'thumbnail', 'active'
+    ]
+    list_filter = ['active']
+    search_fields = ['title']
+
+    def posts_count(self, obj):
+        count = Post.objects.filter(category_id=obj.id).count()
+        return count
+
+    posts_count.allow_tags = True
+    posts_count.short_description = 'No. Posts'
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'created', 'updated']
@@ -12,4 +27,6 @@ class PostAdmin(admin.ModelAdmin):
     class Meta:
         model = Post
 
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Post, PostAdmin)
